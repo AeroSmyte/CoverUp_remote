67f754a3ba49f170152383a7f29ec1b6adb4cb01
Lexi McQueen <alexismcqueen01@gmail.com>
2023-08-10 00:32:13 -0400

    Initial Commit

---
+++ CoverUp/CoverUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverUpApp.swift
+//  CoverUp
+//
+//  Created by Lexi McQueen on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoverUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
