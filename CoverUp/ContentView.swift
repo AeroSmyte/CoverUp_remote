439d0d59666368b3a65c09f7a1d0939e57342efe
Lexi McQueen <alexismcqueen01@gmail.com>
2023-08-11 14:10:39 -0400

    Adding initial screen for app.

--- CoverUp/ContentView.swift
+++ CoverUp/ContentView.swift
@@ -9,14 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors:
+                                                [Color("topBackgroundGradient"), Color("bottomBackgroundGradient")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack(spacing: 50) {
+                Image(uiImage: UIImage(named: "cover-up-text")!)
+                
+                Button {
+                    print("Upload button tapped.")
+                } label: {
+                    Text("Upload Something!")
+                        .bold()
+                        .frame(width: 200, height: 80)
+                        .foregroundColor(Color("Alabaster"))
+                        .background(LinearGradient(gradient: Gradient(colors: [Color("YellowPaleGradient"), Color("YellowGradient")]), startPoint: .leading, endPoint: .trailing))
+                        .clipShape(Capsule())
+                    
+                }
+                
+//                Image(uiImage: UIImage(named: "CoverUpUploadImage")!)
+//                    .resizable()
+//                    .frame(width: 300, height: 400)
+               
+                
+            }
+            .padding()
         }
-        .padding()
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
